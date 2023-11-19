ad09289dadb6ff61c4c62c83e19c4b0ed50cef8c
arhamcodes <arhamtmallick@gmail.com>
2023-11-19 20:58:10 +0500

    edit password in db url

--- models/models.js
+++ models/models.js
@@ -10,13 +10,22 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         min: 6,
-        max: 225
+        max: 225,
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid E-mail'
+        ]
     },
     password: {
         type: String,
         required: true,
         min: 6,
-        max: 225
+        max: 225,
+        match: [
+            /^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]+$/,
+            'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and a special characters'
+        ]
     },
     date: {
         type: Date,
